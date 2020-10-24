d9673e7e0bf8aaa9ab64083605d28079d562e430
AhmadR-coder-max <upworka786@gmail.com>
2020-10-24 18:20:14 +0500

    Generate Models and Migrations.

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -2,5 +2,24 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    # render(:template => 'demo/hello')
+    # render('demo/hello')
+    # render('hello')
   end
+
+  def hello
+    # render('index')
+    @array = [1, 2, 3, 4, 5]
+    @id = params['id'].to_i
+    @page = params[:page].to_i
+  end
+
+  def other_hello
+    redirect_to(:controller => "demo" ,:action => 'index')
+  end
+
+  def lyanda
+    redirect_to('http://www.lyanda.com')
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  root 'demo#index'
+  # get 'demo/index'
+  match ':controller(/:action(/:id))', via: :get
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
