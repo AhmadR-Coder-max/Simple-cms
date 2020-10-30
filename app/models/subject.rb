9cfb237d56b240ef34864c2eaabfd95b1d8395ee
AhmadR-coder-max <upworka786@gmail.com>
2020-10-30 18:16:13 +0500

    Learn ActiveRecord

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,16 @@
 class Subject < ApplicationRecord
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("subjects.position ASC") }
+  scope :newest_first, lambda { order("subjects.created_at DESC") }
+  scope :search, lambda {|query|
+    where(["name LIKE ?", "%#{query}%"])
+  }
+
+  # Same as
+  # def self.active
+  #   where(visible: true)
+  # end
+
 end
