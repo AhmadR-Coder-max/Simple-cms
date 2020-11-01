4244e448fce4d89a2501a0ee3ca7960a13c4c254
AhmadR-coder-max <upworka786@gmail.com>
2020-11-01 20:54:54 +0500

    Learn Simple, One-to-many and Many-to-Many Associations

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,11 @@
 class Page < ApplicationRecord
+  belongs_to :subject
+  # In case you don't follow the rails convention, you name your join table with a different name
+  # Then you can specify your table_name here
+  # has_and_belongs_to_many :admin_users, join_table: "admin_users_pages"
+
+  # has_and_belongs_to_many :admin_users
+
+  has_and_belongs_to_many :editors, class_name: "AdminUser"
+
 end
